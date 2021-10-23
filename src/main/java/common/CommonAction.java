5544eddecbe143383cfaa30b95c4faffea3ff035
texnocobra <ea2UBam9FwjT32V>
2021-10-23 21:13:02 +0700

    Added cleanup browser cookiea and storage

---
+++ src/main/java/common/CommonAction.java
@@ -0,0 +1,24 @@
+package common;
+
+import com.codeborne.selenide.Selenide;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import static common.Config.CLEAR_COOKIES;
+
+public class CommonAction {
+    public static final Logger LOGGER = LoggerFactory.getLogger(CommonAction.class);
+
+
+    public static void clearBrowserCookieAndStorage() {
+        if (CLEAR_COOKIES) {
+            try {
+                Selenide.clearBrowserCookies();
+                Selenide.clearBrowserLocalStorage();
+
+                Selenide.executeJavaScript("window.sessionStorage.clear");
+            } catch (Exception e){
+                LOGGER.error(" Fail to clearBrowserCookieAndStorage() - {}" + e.getMessage());
+            }
+        }
+    }
+}
