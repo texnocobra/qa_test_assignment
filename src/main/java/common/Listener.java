9ab0b5fdd6dfd8efbc704e64d6fca51fb6f10410
texnocobra <ea2UBam9FwjT32V>
2021-10-23 21:07:07 +0700

    Added logger and screenshots to Allure

---
+++ src/main/java/common/Listener.java
@@ -0,0 +1,45 @@
+package common;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
+import org.junit.jupiter.api.extension.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import static common.CommonAction.clearBrowserCookieAndStorage;
+import static org.junit.jupiter.api.extension.ExtensionContext.Namespace.GLOBAL;
+
+public class Listener implements TestWatcher, BeforeAllCallback, AfterEachCallback {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Listener.class);
+
+    @Override
+    public void beforeAll(ExtensionContext extensionContext) {
+        extensionContext.getRoot().getStore(GLOBAL).put(true, false);
+    }
+
+    @Override
+    public void afterEach(ExtensionContext extensionContext) {
+        clearBrowserCookieAndStorage();
+    }
+
+    @Override
+    public void testFailed(ExtensionContext context, Throwable cause){
+        LOGGER.info("Test {} - Failed", context.getTestMethod().get().getName());
+        String screenshotName = context.getTestMethod().get().getName()
+                +String.valueOf(System.currentTimeMillis()).substring(9, 13);
+        LOGGER.info("Trying to trace screenShot...");
+        Selenide.screenshot(screenshotName);
+
+        attachSreenshotToAllure();
+    }
+
+    @Attachment
+    public byte [] attachSreenshotToAllure(){
+        if(WebDriverRunner.hasWebDriverStarted())
+        return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+        else return null;
+    }
+}
